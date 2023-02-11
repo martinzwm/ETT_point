0cef44179d7474a0ad28b6dae23ba1a3ad5c1743
EC2 Default User <ec2-user@ip-10-0-23-166.ec2.internal>
2023-02-11 22:49:54 +0000

    Finish training / testing pipeline

--- regression.py
+++ regression.py
@@ -3,12 +3,8 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 import utils
 import transforms as T
-
-import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 from dataset import *
 
@@ -33,33 +29,27 @@ def get_dataloader():
         target_file='target_downsized.json',
         transforms=get_transform(train=True)
         )
-    dataset_test = CXRDataset(
-        root='/home/ec2-user/data/MIMIC_ETT_annotations', 
-        image_dir='downsized',
-        ann_file='annotations_downsized.json',
-        target_file='target_downsized.json',
-        transforms=get_transform(train=True)
+
+    val_size, test_size = len(dataset) // 3, len(dataset) // 3
+    dataset_train, dataset_val, dataset_test = torch.utils.data.random_split(
+        dataset, [len(dataset)-val_size-test_size, val_size, test_size]
         )
 
-    # split the dataset in train and test set
-    torch.manual_seed(1234)
-    indices = torch.randperm(len(dataset)).tolist()
-    dataset = torch.utils.data.Subset(dataset, indices[:-50])
-    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
-
     # define training and validation data loaders
-    data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=1, shuffle=True, num_workers=4,
+    dataloader_train = torch.utils.data.DataLoader(
+        dataset_train, batch_size=2, shuffle=True, num_workers=4,
         collate_fn=utils.collate_fn)
-    data_loader_test = torch.utils.data.DataLoader(
-        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+    dataloader_val = torch.utils.data.DataLoader(
+        dataset_val, batch_size=2, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
+    dataloader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=2, shuffle=False, num_workers=4,
         collate_fn=utils.collate_fn)
 
-    return data_loader, data_loader_test
+    return dataloader_train, dataloader_val, dataloader_test
 
 
 def get_model():
-    # model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
     model = torch.hub.load('pytorch/vision:v0.10.0', 'fcn_resnet50', pretrained=True)
     
     model = nn.Sequential(
@@ -72,60 +62,74 @@ def get_model():
     return model
 
 
-def train(model, dataset, optimizer, device, epoch):
+def train(model, dataset_train, dataset_val, optimizer, device, epoch):
     model.train()
     for i in range(epoch):
         print("Training epoch: ", i+1, " / ", epoch, " ...")
         
-        for images, targets in dataset:
-            images = images[0].unsqueeze(0)
+        for images, targets in dataset_train:
+            images = torch.stack([image for image in images], dim=0)
             images = images.to(device)
             predicted = model(images)
 
-            bbox = targets[0]['boxes'].squeeze(0)
-            center = torch.tensor([(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]).unsqueeze(0)
-            center = center.to(device)
+            bboxes = torch.stack([target['boxes'].squeeze(0) for target in targets], dim=0)
+            centers = torch.stack(
+                [(bboxes[:, 0] + bboxes[:, 2]) / 2, (bboxes[:, 1] + bboxes[:, 3]) / 2],
+                dim=1
+            )
+            centers = centers.to(device)
 
             # L2 loss
-            loss = F.mse_loss(predicted, center)
+            loss = F.mse_loss(predicted, centers)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
+        # Progress report
+        print("Training set", end=" ")
+        test(model, dataset_train, device)
+        print("Validation set", end=" ")
+        test(model, dataset_val, device)
 
-def test(model, dataset, device):
+
+def test(model, dataset_test, device):
     model.eval()
     dist = []
-    for images, targets in dataset:
-        images = images[0].unsqueeze(0)
+
+    for images, targets in dataset_test:
+        images = torch.stack([image for image in images], dim=0)
         images = images.to(device)
         predicted = model(images)
 
-        bbox = targets[0]['boxes'].squeeze(0)
-        center = torch.tensor([(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]).unsqueeze(0)
-        center = center.to(device)
+        bboxes = torch.stack([target['boxes'].squeeze(0) for target in targets], dim=0)
+        centers = torch.stack(
+            [(bboxes[:, 0] + bboxes[:, 2]) / 2, (bboxes[:, 1] + bboxes[:, 3]) / 2],
+            dim=1
+        )
+        centers = centers.to(device)
 
-        loss = F.mse_loss(predicted, center)
-        loss = torch.sqrt(loss)
-        dist.append(loss.item())
+        with torch.no_grad():
+            # L2 loss
+            loss = F.mse_loss(predicted, centers)
+            loss = torch.sqrt(loss)
+            dist.append(loss.item())
     
     print("Average L2 loss: ", sum(dist) / len(dist))
 
 
 def pipeline():
+    torch.manual_seed(1234)
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    data_loader, data_loader_test = get_dataloader()
+    dataloader_train, dataloader_val, dataloader_test = get_dataloader()
     model = get_model()
     model.to(device)
 
     params = [p for p in model.parameters() if p.requires_grad]
     optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-    train(model, data_loader, optimizer, device, 10)
+    train(model, dataloader_train, dataloader_val, optimizer, device, 2)
 
-    print("Testing on training set ...")
-    test(model, data_loader, device)
     print("Testing on test set ...")
-    test(model, data_loader_test, device)
+    test(model, dataloader_test, device)
 
 
 if __name__ == "__main__":    
