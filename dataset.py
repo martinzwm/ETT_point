768de865a93d7d9a0293b9871fdc2afbd0099b56
EC2 Default User <ec2-user@ip-10-0-23-166.ec2.internal>
2023-03-11 01:23:07 +0000

    Disable rotation

--- dataset.py
+++ dataset.py
@@ -18,7 +18,7 @@ def get_transform(train):
         )) # normalize
     if train:
         transforms.append(T.RandomHorizontalFlip(0.5))
-        transforms.append(T.RandomRotate(10))
+        # transforms.append(T.RandomRotate(10))
         transforms.append(T.RandomTranslate(0.1))
     return T.Compose(transforms)
 
